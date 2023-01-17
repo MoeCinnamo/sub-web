cad31a4a8e7163aabc9627f10bb903e6784aa13d
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2023-01-17 23:20:41 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -425,7 +425,7 @@ export default {
     };
   },
   created() {
-    document.title = "Subscription Converter";
+    document.title = "訂閱轉換";
     this.isPC = this.$getOS().isPc;
 
     // 获取 url cache
